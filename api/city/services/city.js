8478d3fa2317aad98d27911c6f56927acccf3635
Dicky Chan <hinyiuchan@hk01.com>
2021-01-27 19:07:23 +0800

    updates

--- api/city/services/city.js
+++ api/city/services/city.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  find(params, populate) {
+    return strapi.query("city").find(params, []);
+  },
+  search(params) {
+    return strapi.query("city").search(params, []);
+  },
+};
