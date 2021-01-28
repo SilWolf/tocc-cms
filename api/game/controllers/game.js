2836c202bfd884b15ef266cb7b88b72c2940609a
SilWolf <silwolf1121@gmail.com>
2021-01-28 23:52:36 +0800

    feat: api to draft/publish/complete game

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,113 @@
-'use strict';
+"use strict";
+
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    const query = ctx.query;
+    if (ctx.state?.user?.role?.name !== "Dungeon Master") {
+      query["status_ne"] = "draft";
+    }
+
+    let entities;
+    if (query._q) {
+      entities = await strapi.services.game.search(query);
+    } else {
+      entities = await strapi.services.game.find(query);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.game })
+    );
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.game.findOne({ id });
+    if (
+      entity.status === "draft" &&
+      ctx.state?.user?.role?.name !== "Dungeon Master"
+    ) {
+      return ctx.notFound();
+    }
+    return sanitizeEntity(entity, { model: strapi.models.game });
+  },
+
+  count(ctx) {
+    const query = ctx.query;
+    if (ctx.state?.user?.role?.name !== "Dungeon Master") {
+      query["status_ne"] = "draft";
+    }
+
+    if (query._q) {
+      return strapi.services.game.countSearch(ctx.query);
+    }
+    return strapi.services.game.count(ctx.query);
+  },
+
+  async patchToDraft(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.game.findOne({ id });
+    if (entity.status !== "published") {
+      return ctx.badRequest();
+    }
+
+    const updatedEntity = await strapi.services.game.update(
+      { id },
+      {
+        ...entity,
+        status: "draft",
+        publishedAt: null,
+      }
+    );
+
+    return sanitizeEntity(updatedEntity, { model: strapi.models.game });
+  },
+
+  async patchToPublished(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.game.findOne({ id });
+    if (entity.status !== "draft") {
+      return ctx.badRequest();
+    }
+
+    const updatedEntity = await strapi.services.game.update(
+      { id },
+      {
+        ...entity,
+        status: "published",
+        publishedAt: new Date().toISOString(),
+      }
+    );
+
+    return sanitizeEntity(updatedEntity, { model: strapi.models.game });
+  },
+
+  async patchToCompleted(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await trapi.services.game.findOne({ id });
+    if (entity.status !== "published") {
+      return ctx.badRequest();
+    }
+
+    const updatedEntity = await strapi.services.game.update(
+      { id },
+      {
+        ...entity,
+        status: "completed",
+        completedAt: new Date().toISOString(),
+      }
+    );
+
+    return sanitizeEntity(updatedEntity, { model: strapi.models.game });
+  },
+};
