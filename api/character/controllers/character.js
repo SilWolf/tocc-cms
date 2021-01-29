91381fc4c021494484e87d0e57948d7dcc8e719b
SilWolf <silwolf1121@gmail.com>
2021-01-29 11:03:49 +0800

    feat: api to get my characters

--- api/character/controllers/character.js
+++ api/character/controllers/character.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async me(ctx) {
+    const user = ctx.state?.user;
+    if (!user) {
+      return ctx.unauthorized();
+    }
+
+    const query = {
+      player: user.id,
+    };
+
+    entities = await strapi.services.game.find(query);
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.game })
+    );
+  },
+};
