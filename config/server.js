f429e3a9fb98e3c9b5620f34833e7e75e6d66707
SilWolf <silwolf1121@gmail.com>
2021-07-18 16:40:29 +0800

    feat: google auth

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '95ebdee00a4a59aa3810a1da2c9b02ca'),
+      secret: env("ADMIN_JWT_SECRET", "95ebdee00a4a59aa3810a1da2c9b02ca"),
     },
   },
+  url: env("API_ENDPOINT", "http://localhost:1337"),
 });
